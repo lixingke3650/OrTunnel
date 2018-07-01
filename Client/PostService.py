fdf0f3c75466374f365f55ed7b586c9eaab0b82f
licheng <licheng@hanbinnoMacBook-puro.local>
2018-07-01 14:28:53 +0800

    update to v0.43

--- Client/PostService.py
+++ Client/PostService.py
@@ -42,7 +42,7 @@ class PostService():
             self._WorkerThread.start()
             return True
         except Exception as e:
-            globals.G_Log.error('Worker Service Start error! [TunnelWorker.py:start] --> %s' %e)
+            globals.G_Log.error('Worker Service Start error! [PostService.py:start] --> %s' %e)
             return False
 
     def stop(self):
@@ -68,31 +68,39 @@ class PostService():
     def abolishworker(self, tunnelworker):
         '''worker stop'''
 
-        try:
-            if (tunnelworker._isEnable == True):
-                if (tunnelworker._Client_Server_Socket != None):
-                    tunnelworker._Client_Server_Socket.shutdown(socket.SHUT_RDWR)
-                    tunnelworker._Client_Server_Socket.close()
-                    tunnelworker._Client_Server_Socket = None
-                if (tunnelworker._Client_App_Socket != None):
+        # try:
+        if (tunnelworker._isEnable == True):
+            if (tunnelworker._Client_App_Socket != None):
+                try:
                     tunnelworker._Client_App_Socket.shutdown(socket.SHUT_RDWR)
                     tunnelworker._Client_App_Socket.close()
-                    tunnelworker._Client_App_Socket = None
-                try:
-                    ret = self._tunnelWorksManager('del', tunnelworker)
-                    tunnelworker._isEnable = False
                 except Exception as e:
-                    # tunnel worker delete error
-                    # 可能存在多重删除的情况，输出DEBUG日志
-                    globals.G_Log.debug('Worker abolish delete error! [TunnelWorker.py:abolishworker] --> %s' %e)
-        except EnvironmentError as e:
-            # socket二次关闭，输出DEBUG日志
-            globals.G_Log.debug('Worker abolish EnvironmentError! [TunnelWorker.py:abolishworker] --> %s' %e)
-        except AttributeError as e:
-            # socket被关闭后无法读写，输出DEBUG日志
-            globals.G_Log.debug('Worker abolish AttributeError! [TunnelWorker.py:abolishworker] --> %s' %e)
-        except Exception as e:
-            globals.G_Log.error('Worker abolish error! [TunnelWorker.py:abolishworker] --> %s' %e)
+                    globals.G_Log.debug('Worker abolish _Client_App_Socket error! [PostService.py:abolishworker] --> %s' %e)
+                finally:
+                    tunnelworker._Client_App_Socket = None
+            if (tunnelworker._Client_Server_Socket != None):
+                try:
+                    tunnelworker._Client_Server_Socket.shutdown(socket.SHUT_RDWR)
+                    tunnelworker._Client_Server_Socket.close()
+                except Exception as e:
+                    globals.G_Log.debug('Worker abolish _Client_Server_Socket error! [PostService.py:abolishworker] --> %s' %e)
+                finally:
+                    tunnelworker._Client_Server_Socket = None
+            try:
+                ret = self._tunnelWorksManager('del', tunnelworker)
+                tunnelworker._isEnable = False
+            except Exception as e:
+                # tunnel worker delete error
+                # 可能存在多重删除的情况，输出DEBUG日志
+                globals.G_Log.debug('Worker abolish delete error! [PostService.py:abolishworker] --> %s' %e)
+        # except EnvironmentError as e:
+        #     # socket二次关闭，输出DEBUG日志
+        #     globals.G_Log.debug('Worker abolish EnvironmentError! [PostService.py:abolishworker] --> %s' %e)
+        # except AttributeError as e:
+        #     # socket被关闭后无法读写，输出DEBUG日志
+        #     globals.G_Log.debug('Worker abolish AttributeError! [PostService.py:abolishworker] --> %s' %e)
+        # except Exception as e:
+        #     globals.G_Log.error('Worker abolish error! [PostService.py:abolishworker] --> %s' %e)
 
     def workerlisten(self):
         ''' '''
@@ -102,7 +110,7 @@ class PostService():
                 launchthread = threading.Thread(target = self.launchworker, args = (tunnelworker,))
                 launchthread.start()
         except Exception as e:
-            globals.G_Log.error('Worker abolish error! [TunnelWorker.py:workerlisten] --> %s' %e)
+            globals.G_Log.error('Worker abolish error! [PostService.py:workerlisten] --> %s' %e)
 
     def launchworker(self, tunnelworker):
         ''' '''
@@ -111,10 +119,10 @@ class PostService():
         if (globals.G_TUNNEL_METHOD == 'TCP'):
             try:
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, globals.G_SOCKET_UDP_SEND_BUFFERSIZE)
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, globals.G_SOCKET_UDP_RECV_BUFFERSIZE)
-                globals.G_Log.info('socket send buffer size: %d' %(sock.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)))
-                globals.G_Log.info('socket recv buffer size: %d' %(sock.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)))
+                # sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, globals.G_SOCKET_UDP_SEND_BUFFERSIZE)
+                # sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, globals.G_SOCKET_UDP_RECV_BUFFERSIZE)
+                # globals.G_Log.info('socket send buffer size: %d' %(sock.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)))
+                # globals.G_Log.info('socket recv buffer size: %d' %(sock.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)))
 
                 tunnelworker._Client_Server_Socket = sock
                 serveraddress = (tunnelworker._TunnelGroup._TargetIP, tunnelworker._TunnelGroup._TargetPort)
@@ -131,7 +139,7 @@ class PostService():
                 stocthread.start()
                 tunnelworker._isEnable = True
             except Exception as e:
-                globals.G_Log.error('Worker Launch error! [TunnelWorker.py:launchworker] --> %s' %e)
+                globals.G_Log.error('Worker Launch error! [PostService.py:launchworker] --> %s' %e)
         
         elif (globals.G_TUNNEL_METHOD == 'UDP'):
             try:
@@ -152,7 +160,7 @@ class PostService():
                 ctosthread.start()
                 stocthread.start()
             except Exception as e:
-                globals.G_Log.error('Worker Launch error! [TunnelWorker.py:launchworker] --> %s' %e)
+                globals.G_Log.error('Worker Launch error! [PostService.py:launchworker] --> %s' %e)
 
 
     def ctosrun_tcp(self, tunnelworker):
